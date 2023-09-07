49aea73c45b5cc2fe3a0aff8bb2ac7fa0d4fbfb5
mattummal <jehmatts@gmail.com>
2023-09-07 22:53:29 +0200

    chore: no-html

--- apps/mattummal.com/.eslintrc.js
+++ apps/mattummal.com/.eslintrc.js
@@ -1,5 +1,11 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
+  rules: {
+    '@next/next/no-html-link-for-pages': [
+      2,
+      path.join(__dirname, 'custom-pages'),
+    ],
+  },
   settings: {
     next: {
       rootDir: 'apps/mattummal.com',
