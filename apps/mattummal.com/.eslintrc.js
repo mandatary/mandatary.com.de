a23e8fdfddc3d1c3473d1afe8870c7a727f123ff
mattummal <jehmatts@gmail.com>
2023-09-07 22:32:08 +0200

    eslint updated

--- apps/mattummal.com/.eslintrc.js
+++ apps/mattummal.com/.eslintrc.js
@@ -1,5 +1,9 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
+  settings: {
+    next: {
+      rootDir: 'apps/mattummal.com',
+    },
   root: true,
   extends: ['next-typescript'],
   parserOptions: {
