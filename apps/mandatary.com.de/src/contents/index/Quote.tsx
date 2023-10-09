665f53fa7ec39cd25d23701e19d5544a3fad02ec
mattummal <jehmatts@gmail.com>
2023-10-09 19:07:21 +0200

    chore: removed . from the Quote

--- apps/mandatary.com.de/src/contents/index/Quote.tsx
+++ apps/mandatary.com.de/src/contents/index/Quote.tsx
@@ -73,7 +73,7 @@ function Quote() {
                 'dark:bg-slate-800'
               )}
             />
-            mandate.
+            mandate
           </strong>
         </span>
       </span>
