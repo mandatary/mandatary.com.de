a5247ff53f334544dd316cfae7d5b7b97c7d5c03
mattummal <jehmatts@gmail.com>
2023-10-16 11:12:16 +0200

    chore: LOGO-UPDATED

--- apps/mandatary.com.de/src/components/Logo.tsx
+++ apps/mandatary.com.de/src/components/Logo.tsx
@@ -6,11 +6,12 @@ interface LogoProps {
 
 function Logo({ active = false }: LogoProps) {
   return (
-    <div className={clsx('flex items-center gap-1.5 font-[1000] leading-none')}>
+    <div className={clsx('flex items-center gap-1.5 text-xl font-bold')}>
       <div
         className={clsx(
-          'border-box flex h-8 w-8 items-center justify-center rounded-xl border-2',
-          'sm:h-6 sm:w-6 sm:rounded-lg',
+          'bg-accent-600 text-white',
+          'h-13 w-13 sm:h-11 sm:w-11',
+          'flex items-center justify-center',
           [
             active
               ? 'border-accent-600 bg-accent-600 dark:border-accent-500 dark:bg-accent-500'
@@ -18,15 +19,9 @@ function Logo({ active = false }: LogoProps) {
           ]
         )}
       >
-        <div
-          className={clsx(
-            'h-3.5 w-0.5 rotate-12 rounded-full',
-            'sm:h-3 sm:w-0.5',
-            [active ? 'bg-white' : 'bg-accent-600 dark:bg-accent-400']
-          )}
-        />
+        mDy
       </div>
-      <div className={clsx('-mt-1 hidden text-xl', 'sm:block')}>
+      <div className={clsx('-mt-1 text-2xl', 'sm:block')}>
         <span className={clsx('text-slate-900', 'dark:text-slate-200')}>
           man
         </span>
